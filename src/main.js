07fdba850b696738c6231f413d5291e58f3580e7
kyy108516 <961601215@qq.com>
2018-12-20 16:54:23 +0800

    解决express跨域连接

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 import store from './store/index'
 
-
+var url="http://localhost:3000"
+axios.get(url+'/users')
+  .then(function (response) {
+  console.log(response);
+})
+  .catch(function (error) {
+    console.log(error)
+  })
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
