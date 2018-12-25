1c311b82248c43404c4f50dc9481c123d81f41fa
kyy108516 <961601215@qq.com>
2018-12-25 16:42:08 +0800

    美化登录页，完成登录功能

--- src/components/login.vue
+++ src/components/login.vue
@@ -9,14 +9,14 @@
       <p>打破传统模式 解决核心难题 让工作更高效 让管理更有效</p>
       <p>全方位多角度解读市场核心需求，为中国车队保驾护航</p>
     </div>
-    <el-form label-width="80px" :model="loginForm">
+    <el-form label-width="80px" :model="loginForm" :rules="rules" ref="loginForm">
       <div class="form">
         <h1 class="login_h1">欢迎登陆</h1>
         <div class="form_item">
-          <el-form-item class="login_item">
+          <el-form-item class="login_item" prop="username">
             <el-input v-model="loginForm.username" placeholder="请输入您的账号"></el-input>
           </el-form-item>
-          <el-form-item class="login_item" style="margin-top: 30px">
+          <el-form-item class="login_item" style="margin-top: 30px" prop="password">
             <el-input v-model="loginForm.password" placeholder="请输入您的密码"></el-input>
           </el-form-item>
           <el-button type="primary" @click="login" class="form_login">登录</el-button>
@@ -37,7 +37,14 @@
           username: "",
           password: "",
         },
-        rules: {}
+        rules: {
+          username: [
+            {required: true, message: '请输入账号', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'}
+          ]
+        }
       }
     },
     methods: {
@@ -47,11 +54,21 @@
         axios.get(url + '/users/getUser?user_id=' + data.username + '&user_password=' + data.password)
           .then(function (response) {
             console.log(response);
+            if (response.data.msg === '用户不存在') {
+              window.alert("用户不存在")
+            } else if (response.data.msg === '密码错误') {
+              window.alert("密码错误")
+            } else if (response.data.msg === '登录成功') {
+              jump(home)
+            }
           })
           .catch(function (error) {
             console.log(error)
           })
 
+      },
+      jump(index) {
+        this.$router.push('index')
       }
     }
   }
@@ -115,14 +132,16 @@
     text-align: center;
 
   }
-  .logo1{
+
+  .logo1 {
     width: 500px;
     height: 400px;
     position: absolute;
     left: 15%;
     bottom: 24%;
   }
-  .biaoti{
+
+  .biaoti {
     font-family: PingFang-SC-Regular;
     font-size: 50px;
     color: #FFFFFF;
@@ -130,7 +149,8 @@
     line-height: 64px;
     margin-top: 20px;
   }
-  p{
+
+  p {
     font-family: PingFangSC-Light;
     font-size: 19.2px;
     color: #FFFFFF;
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,41 @@ import vuex from 'vuex'
 
 Vue.use(vuex)
 
-const state={
+const state = {
+  token: window.sessionStorage.getItem('token'),
+  username: ""
+};
 
-}
+const mutations = {
+  LOGIN: (state, data) => {
+    state.token = data;
+    window.sessionStorage.removeItem('token');
+  },
+  LOGOUT: (state) => {
+    state.token = null;
+    window.sessionStorage.removeItem('token');
+  },
+  USERNAME: (state, data) => {
+    state.username=data;
+    window.sessionStorage.setItem('username',data);
+  }
+};
+
+const actions={
+  UserLogin({commit},data){
+    commit('LOGIN',data);
+  },
+  UserLogout({commit}){
+    commit('LOGOUT');
+  },
+  UserName({commit},data){
+    commit('USERNAME',data);
+  }
+};
 
 
 export default new vuex.Store({
   state,
+  mutations,
+  actions
 })
