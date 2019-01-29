57454d7c351e1cf06de28bae59ccf56588ced101
kyy108516 <961601215@qq.com>
2019-01-29 18:03:50 +0800

    修改车辆列表

--- src/page/car/carlist.vue
+++ src/page/car/carlist.vue
@@ -11,41 +11,37 @@
       </div>
     </div>
     <div class="list-tit">
-      <el-tabs>
+      <el-tabs v-model="typecard" @tab-click="handleClick">
         <el-tab-pane label="所有车辆"></el-tab-pane>
         <el-tab-pane label="运营中车辆"></el-tab-pane>
+        <el-tab-pane label="库存车辆"></el-tab-pane>
       </el-tabs>
     </div>
     <div class="query-bar">
       <el-form :inline="true" :label-position="right" label-width="80px">
         <el-form-item label="车牌号">
-          <el-input></el-input>
+          <el-input v-model="queryData.license"></el-input>
         </el-form-item>
         <el-form-item label="车型">
-          <el-select v-model="data" filterable placeholder="车型">
+          <el-select v-model="queryData.model" filterable placeholder="车型">
+            <el-option :label="'全部'" :value="''"></el-option>
             <el-option
               v-for="item in cartypeData"
               :key="item.id"
               :label="item.model"
-              :value="item.id">
+              :value="item.model">
             </el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="车架号">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="车型">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="车型">
-          <el-input></el-input>
+          <el-input v-model="queryData.vin"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="info" plain="">查询</el-button>
+          <el-button type="info" plain="" @click="getData">查询</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <div class="view_table">
+    <div class="view_table" v-if="typecard==0">
       <el-table ref="multipleTable" :data="carData.slice((currpage - 1) * pagesize, currpage * pagesize)" tooltip-effect="dark" style="width: 100%"
                 @selection-change="handleSelectionChange" highlight-current-row>
         <el-table-column type="selection" width="55"></el-table-column>
@@ -57,6 +53,57 @@
         <el-table-column prop="license" label="车牌号" ></el-table-column>
         <el-table-column prop="vin" label="车架号" show-overflow-tooltip></el-table-column>
         <el-table-column prop="model" label="车型" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="state" label="状态" show-overflow-tooltip></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small" @click="deleteCar(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :page-sizes="[10, 20, 30, 40]" :page-size="pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="this.carData.length"></el-pagination>
+    </div>
+    <div class="view_table" v-if="typecard==1">
+      <el-table ref="multipleTable" :data="carData.slice((currpage - 1) * pagesize, currpage * pagesize)" tooltip-effect="dark" style="width: 100%"
+                @selection-change="handleSelectionChange" highlight-current-row>
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" label="编号">
+          <template slot-scope="scope">
+            <span>{{scope.row.id}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="license" label="车牌号" ></el-table-column>
+        <el-table-column prop="vin" label="车架号" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="model" label="车型" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="state" label="状态" show-overflow-tooltip></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small" @click="deleteCar(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :page-sizes="[10, 20, 30, 40]" :page-size="pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="this.carData.length"></el-pagination>
+    </div>
+    <div class="view_table" v-if="typecard==2">
+      <el-table ref="multipleTable" :data="carData.slice((currpage - 1) * pagesize, currpage * pagesize)" tooltip-effect="dark" style="width: 100%"
+                @selection-change="handleSelectionChange" highlight-current-row>
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" label="编号">
+          <template slot-scope="scope">
+            <span>{{scope.row.id}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="license" label="车牌号" ></el-table-column>
+        <el-table-column prop="vin" label="车架号" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="model" label="车型" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="state" label="状态" show-overflow-tooltip></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button type="text" size="small">查看</el-button>
@@ -80,6 +127,13 @@
     name: "carlist",
     data() {
       return {
+        queryData:{
+          license:'',
+          vin:'',
+          model:'',
+          state:''
+        },
+        typecard:0,
         carData: [],
         cartypeData:[],
         currpage:1,
@@ -93,18 +147,26 @@
     methods: {
       getData() {
         var url = "http://localhost:3000";
-        function getcarData(){
-          return axios.get(url+'/car/query')
-        }
-        function getcartypeData(){
-          return axios.get(url + '/cartype/queryAll')
-        }
-        axios.all([getcarData(),getcartypeData()])
-          .then(axios.spread((getcarData,getcartypeData) =>{
-            this.carData=getcarData.data.data
-            this.cartypeData=getcartypeData.data.data
-          }
-          ))
+        axios.post(url+'/car/query',this.queryData)
+          .then(response=>{
+            if (response.data.code=='200') {
+              this.carData = response.data.data
+            }
+            if (response.data.code=='1'){
+              this.carData=[]
+            }
+          })
+          .catch(error=>{
+            console.log(error);
+          });
+        axios.get(url+'/cartype/queryAll')
+          .then(response=>{
+              this.cartypeData = response.data.data
+          })
+          .catch(error=>{
+            console.log(error);
+          })
+      },
       },
       handleSelectionChange(val) {
         this.multipleSelection = val;
@@ -114,6 +176,9 @@
       },
       handleSizeChange(psize) {
         this.pagesize = psize;
+      },
+      handleClick(tab, event) {
+        this.typecard=tab.index
       },
       deleteCar(id){
         var url = "http://localhost:3000";
@@ -126,7 +191,6 @@
             console.log(error)
           })
       },
-    }
   }
 </script>
 
